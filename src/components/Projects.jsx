cb074f301cf03292aeb88e706a8400b86dccf9b1
LuYomayel <l.yomayel@mgail.com>
2024-07-22 18:07:17 +1000

    ok

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,36 +1,34 @@
-
 function Projects() {
     // Aquí puedes agregar tus Projects destacados
     const personalProjects = [
         { 
-            name: "Mobile App for Personal Trainers",
-            type: "Mobile Application",
+            name: "EaseTrain - Web Application for Personal Trainers",
+            type: "Web Application",
             dates: "Aug 2023 - Present",
-            description: "Developing a mobile app for personal trainers to send personalized workout routines. Features include progress tracking and integrated chat for trainer-student communication.",
-            url: "" // Add URL if available
+            description: "Developed the first version of EaseTrain, a web application for personal trainers. Features include personalized workout routines, progress tracking, and integrated chat for trainer-student communication. Utilized React, Node.js, MongoDB, and AWS for development and deployment.",
+            url: "trainease.luciano-yomayel.com" // Add URL if available
         },
         { 
             name: "Secondary School Course Schedule Management Platform",
             type: "Web Application",
             dates: "March 2023 - Present",
             description: "Developed a web application for managing course schedules, utilized by over 500 students. Employed Angular for front-end, NestJS for back-end, and MongoDB for data management. Deployed the application on AWS EC2, achieving high availability and uptime.",
-            url: "https://demo.gestion-horarios.com"
+            // url: "https://demo.gestion-horarios.com"
         },
         { 
             name: "Handball Statistics Page",
             type: "Web Application",
             dates: "May 2023 - Jun 2023",
             description: "Created a web application for displaying handball player statistics and fair play tables. Attracted over 10k monthly visits, becoming a popular resource for handball enthusiasts.",
-            url: "https://goleadores.handball-metropolitano.com/"
+            // url: "https://goleadores.handball-metropolitano.com/"
         },
-        
     ];
 
     return (
         <section>
             <p className="medium-title">Projects</p>
             {personalProjects.map(project => (
-                <div key={project.name} className=" project-div card">
+                <div key={project.name} className="project-div card">
                     {project.url ? (
                         <a href={project.url}>
                             <p className="project-name">{project.name}</p>
@@ -51,7 +49,5 @@ function Projects() {
         </section>
     );
 }
-
-
 
 export default Projects;
